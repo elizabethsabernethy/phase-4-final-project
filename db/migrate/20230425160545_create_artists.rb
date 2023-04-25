56ead5a5efe4e6a3e214fd62c253ac40b44d8d61
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-25 12:21:17 -0400

    adds table data

--- db/migrate/20230425160545_create_artists.rb
+++ db/migrate/20230425160545_create_artists.rb
@@ -1,7 +1,9 @@
 class CreateArtists < ActiveRecord::Migration[6.1]
   def change
     create_table :artists do |t|
-
+      t.string :name
+      t.string :username
+      t.string :password_digest
       t.timestamps
     end
   end
