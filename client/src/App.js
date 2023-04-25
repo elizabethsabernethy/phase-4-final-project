2f06333a116bb43ec4f0eadeae1525f73f62806c
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-25 15:29:08 -0400

    adds nav bar components with route and switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
 import Home from "./Home";
 import Museums from "./Museums";
+import NavBar from "./NavBar";
+import {Route, Switch} from "react-router-dom";
+import Paintings from "./Paintings";
 
 function App() {
   return (
     <div>
-      <Home />
-      <Museums />
+        <NavBar />
+      <Switch>
+        <Route exact path="/museums">
+          <Museums/>
+        </Route>
+        <Route exact path="/paintings">
+          <Paintings/>
+        </Route>
+        <Route exact path="/my-art">
+          {/* ADD MY ART HERE */}
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
