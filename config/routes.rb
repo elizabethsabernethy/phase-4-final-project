4061443a47c3809c64065e64c9a9efc56b2cc705
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-25 17:40:01 -0400

    adds nested resource routing for paintings in museums

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :artists
   resources :paintings
-  resources :museums
+  resources :museums do
+    resources :paintings, only: [:index, :show]
+  end
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
