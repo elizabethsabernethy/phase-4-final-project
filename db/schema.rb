d3aa7b519fee67aef1a3b78f13bf8a7a1637de22
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-25 11:58:26 -0400

    migrates paintings table which updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_25_113554) do
+ActiveRecord::Schema.define(version: 2023_04_25_154043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "museums", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "paintings", force: :cascade do |t|
+    t.string "title"
+    t.string "img_url"
+    t.string "description"
+    t.integer "artist_id"
+    t.integer "museum_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
