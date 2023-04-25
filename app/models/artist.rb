e2f9eb9bef25ecba338007b4556c480c3a0f297d
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-25 13:16:06 -0400

    adds dependent desotry for paintings

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,5 @@
 class Artist < ApplicationRecord
-    has_many :paintings 
+    has_many :paintings, dependent: :destroy
     has_many :museums, through: :paintings 
     validates :username, presence: true, uniqueness: true
 end
--- app/models/museum.rb
+++ app/models/museum.rb
@@ -1,5 +1,5 @@
 class Museum < ApplicationRecord
-    has_many :paintings
+    has_many :paintings, dependent: :destroy
     has_many :artists, through: :paintings
     validates :name, presence: true, uniqueness: true
 end
