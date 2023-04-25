da28e3189031d0c3fbde2bc7adf97b2c93aa0d95
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-25 11:38:19 -0400

    sets state in useeffect and maps museums

--- client/src/Museums.js
+++ client/src/Museums.js
@@ -6,12 +6,16 @@ function Museums(){
     useEffect(()=>{
         fetch('http://localhost:3000/museums')
         .then((resp) => resp.json())
-        .then((museums) => console.log(museums))
+        .then((museums) => setMuseums(museums))
     },[])
 
     return(
         <div>
-            
+            {museums.map((museum)=>{
+                return <div className="museum" key={museum.id}>
+                    <h3>{museum.name}</h3>
+                </div>
+            })}
         </div>
     )
 }
