05e8d4282c2633d39a2f1034bb14e9660ed5bdfb
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-25 12:08:15 -0400

    adds resources for artsits

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :artists
   resources :paintings
   resources :museums, only: [:create, :index, :show]
   # Routing logic: fallback requests for React Router.
