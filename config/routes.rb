86a0e4bf77ed78ac7d58f094f5c86298836c1219
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-25 14:37:25 -0400

    runs npm install to update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :artists
   resources :paintings
-  resources :museums, only: [:create, :index, :show]
+  resources :museums
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
