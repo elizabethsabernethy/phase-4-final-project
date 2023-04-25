32a9238edc4fbde5bf6926602bfcc2d8b6ac2c69
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-25 17:26:16 -0400

    adds painting state and maps over it displaying name, image, and artist/ museum info

--- client/src/Paintings.js
+++ client/src/Paintings.js
@@ -1,7 +1,25 @@
-function Paintings(){
-    return(
-        <div>
+import { useEffect, useState } from "react";
 
+function Paintings(){
+    const[paintings, setPaintings] = useState([])
+
+    useEffect(()=>{
+        fetch('http://localhost:3000/paintings')
+        .then((resp) => resp.json())
+        .then((paintings) => setPaintings(paintings))
+    },[])
+
+    return(
+        <div id="paintings-container">
+            {paintings.map((painting)=>{
+                return <div className="painting" key={painting.id}>
+                    <h3 className="painting-title">{painting.title}</h3>
+                    <h4>By {painting.artist.name}</h4>
+                    <p className="painting-description">{painting.description}</p>
+                    <img className="painting-img" src={painting.img_url} alt={painting.title} width="350px"></img>
+                    <h4>Displayed at {painting.museum.name}</h4>
+                </div>
+            })}
         </div>
     )
 }
