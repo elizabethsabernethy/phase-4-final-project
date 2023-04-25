57c6b80b09034e096eeb85f0f662c0fbe52dd904
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-25 11:31:22 -0400

    changes has many through from collectors to artists

--- app/models/museum.rb
+++ app/models/museum.rb
@@ -1,5 +1,5 @@
 class Museum < ApplicationRecord
     # has_many :paintings
-    # has_many :collectors, through: :paintings
+    # has_many :artists, through: :paintings
     validates :name, presence: true, uniqueness: true
 end
