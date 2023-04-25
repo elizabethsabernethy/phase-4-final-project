8cdb93d45ea31ab912541a5b1255bd79542c859d
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-25 14:41:46 -0400

    adds classnames and other attributes of museum

--- client/src/Museums.js
+++ client/src/Museums.js
@@ -13,7 +13,10 @@ function Museums(){
         <div>
             {museums.map((museum)=>{
                 return <div className="museum" key={museum.id}>
-                    <h3>{museum.name}</h3>
+                    <h3 className="museum-name">{museum.name}</h3>
+                    <h4 className="museum-location">{museum.location}</h4>
+                    <img className="museum-img" src={museum.img_url} alt={museum.name} width="350px"></img>
+                    <button className="view-paintings">View Paintings</button>
                 </div>
             })}
         </div>
