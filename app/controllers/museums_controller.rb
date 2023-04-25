42dae75c4e616c57b7f00a681574e93eec527652
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-25 08:03:20 -0400

    adds strong params and built out create method

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -3,7 +3,22 @@ class MuseumsController < ApplicationController
 rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_response
 
+    def index
+    end
+
+    def show
+    end
+
+    def create
+        museum = Museum.create!(museum_params)
+        render json: museum, status: :created
+    end
+
     private
+
+    def museum_params
+        params.permit(:name)
+    end
 
     def render_invalid_response(invalid)
         render json: { error: invalid.record.errors.messages}, status: :unprocessable_entity
