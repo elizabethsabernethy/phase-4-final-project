29e71c9552a417b1cd1fb25c5bfcdaa7e615ef10
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-25 11:55:53 -0400

    adds table data

--- db/migrate/20230425154043_create_paintings.rb
+++ db/migrate/20230425154043_create_paintings.rb
@@ -1,7 +1,11 @@
 class CreatePaintings < ActiveRecord::Migration[6.1]
   def change
     create_table :paintings do |t|
-
+      t.string :title
+      t.string :img_url
+      t.string :description
+      t.integer :artist_id
+      t.integer :museum_id
       t.timestamps
     end
   end
