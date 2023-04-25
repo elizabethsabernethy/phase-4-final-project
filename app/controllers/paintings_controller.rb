41292ec4a601160fb77e61583b48804557de4fbc
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-25 12:04:28 -0400

    adds strong params, and built out index, show and create methods

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -3,6 +3,28 @@ class PaintingsController < ApplicationController
 rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_response
 
+
+    def index
+        paintings = Painting.all.order(name: :asc)
+        render json: paintings
+    end
+
+    def show
+        painting = Painting.find(params[:id])
+        render json: painting
+    end
+
+    def create
+        painting = painting.create!(painting_params)
+        render json: painting, status: :created
+    end
+
+    private
+
+    def painting_params
+        params.permit(:title, :img_url, :description)
+    end
+
     def render_invalid_response(invalid)
         render json: { error: invalid.record.errors.messages}, status: :unprocessable_entity
     end
