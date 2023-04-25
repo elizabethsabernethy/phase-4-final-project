f53525df19670cfa2d52c91b4cd373e9639bf223
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-25 12:22:39 -0400

    migrated artists table which updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_25_154043) do
+ActiveRecord::Schema.define(version: 2023_04_25_160545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "museums", force: :cascade do |t|
     t.string "name"
