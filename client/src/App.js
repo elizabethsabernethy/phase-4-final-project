6000f7f1558bc0355a3a81219b7244941be00ef8
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-25 17:10:54 -0400

    adds proper imports and components for navlink and recact routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,19 @@
 import Home from "./Home";
 import Museums from "./Museums";
 import NavBar from "./NavBar";
-import {Route, Switch} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Paintings from "./Paintings";
 
 function App() {
   return (
     <div>
         <NavBar />
-      <Switch>
-        <Route exact path="/museums">
-          <Museums/>
-        </Route>
-        <Route exact path="/paintings">
-          <Paintings/>
-        </Route>
-        <Route exact path="/my-art">
-          {/* ADD MY ART HERE */}
-        </Route>
-        <Route exact path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/museums" element={<Museums/>}/>
+        <Route path="/paintings" element={<Paintings/>}/>
+        <Route path="/my-art" element={<Museums/>}/>
+        <Route path="/" element={<Home />}/>
+      </Routes>
     </div>
   );
 }
--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -7,54 +7,35 @@ function NavBar(){
         width: "70px",
         padding: "12px",
         margin: "0 6px 6px",
-        background: "rgb(69, 88, 62)",
-        textDecoration: "none",
-        color: "white",
+        color: "antiquewhite",
+        textAlign: "center",
+        textDecoration:"none",
+        background: "rgb(2, 54, 59)"
       };
 
     return(
         <div className="navbar">
             <NavLink
             to="/"
-            exact
             style={linkStyles}
-            activeStyle={{
-                background:'white',
-                color: 'rgb(69, 88, 62)'
-            }}
             >
                 Home
             </NavLink>
             <NavLink
             to="/museums"
-            exact
             style={linkStyles}
-            activeStyle={{
-                background:'white',
-                color: 'rgb(69, 88, 62)'
-            }}
             >
                 Museums
             </NavLink>
             <NavLink
             to="/paintings"
-            exact
             style={linkStyles}
-            activeStyle={{
-                background:'white',
-                color: 'rgb(69, 88, 62)'
-            }}
             >
                 Paintings
             </NavLink>
             <NavLink
             to="/my-art"
-            exact
             style={linkStyles}
-            activeStyle={{
-                background:'white',
-                color: 'rgb(69, 88, 62)'
-            }}
             >
                 My Art
             </NavLink>
