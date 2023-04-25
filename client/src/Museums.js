969344a80c24f8c0c3ea1623c908cad946276ca0
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-25 08:45:51 -0400

    adds museums state and useeffect to fetch all museums

--- client/src/Museums.js
+++ client/src/Museums.js
@@ -1,8 +1,17 @@
+import { useEffect, useState } from "react";
 
 function Museums(){
+    const[museums, setMuseums] = useState([])
+
+    useEffect(()=>{
+        fetch("http://127.0.0.1:3000/museums")
+        .then((resp) => resp.json())
+        .then((museums) => console.log(museums))
+    },[])
+
     return(
         <div>
-
+            
         </div>
     )
 }
