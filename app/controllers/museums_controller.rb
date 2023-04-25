0578892281cc7e8ee5d5bfea49063cd85d9fb372
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-25 08:01:29 -0400

    adds private methods to render errors for invalid/ not found records

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -1,2 +1,15 @@
 class MuseumsController < ApplicationController
+
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_response
+
+    private
+
+    def render_invalid_response(invalid)
+        render json: { error: invalid.record.errors.messages}, status: :unprocessable_entity
+    end
+
+    def render_not_found_response
+        render json: { error: "Museum not found" }, status: :not_found
+      end
 end
