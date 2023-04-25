0bd32ec028c1f853d0ecfb38c1becb302175c2c9
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-25 11:52:43 -0400

    adds belongs to statments and validations

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,2 +1,7 @@
 class Painting < ApplicationRecord
+    belongs_to :museum
+    belongs_to :artist 
+    validates :title, presence: true, uniqueness: true
+    validates :img_url, presence: true
+    validates :description, presence: true
 end
