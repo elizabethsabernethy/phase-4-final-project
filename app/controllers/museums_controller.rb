57b0275e8c742bc7389c1155830c65b2ad33e220
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-25 12:20:24 -0400

    uncomments includes for show and index and adds to create method

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -5,17 +5,17 @@ rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_response
 
     def index
         museums = Museum.all.order(name: :asc)
-        render json: museums #, include: :paintings
+        render json: museums, include: :paintings
     end
 
     def show
         museum = Museum.find(params[:id])
-        render json: museum #, include: :paintings
+        render json: museum, include: :paintings
     end
 
     def create
         museum = Museum.create!(museum_params)
-        render json: museum, status: :created
+        render json: museum, include: :paintings, status: :created
     end
 
     private
