05e9e60d075df1c67e7e826c115b73f4a295f5a9
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-25 12:00:53 -0400

    adds render error responses for invliad/not found records and rescue from statments

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,2 +1,13 @@
 class PaintingsController < ApplicationController
+
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_response
+
+    def render_invalid_response(invalid)
+        render json: { error: invalid.record.errors.messages}, status: :unprocessable_entity
+    end
+
+    def render_not_found_response
+        render json: { error: "Painting not found" }, status: :not_found
+      end
 end
