ce172e19b6a5ebe9f35c345049d94e71601c2640
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-25 07:20:08 -0400

    adds header and paragraph

--- client/src/Home.js
+++ client/src/Home.js
@@ -3,7 +3,8 @@
 function Home(){
     return(
         <div>
-
+            <h1>The Art of Collection</h1>
+            <p>Welcome to the Art of Collection, where you can view some of the worlds most famous paintings, find out which museum they belong to, and start collecting if you feel so inclined</p>
         </div>
     )
 }
