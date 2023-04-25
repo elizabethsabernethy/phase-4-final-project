e4d557af06300410995f12acd49fe9d03ebe76fa
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-25 12:13:15 -0400

    adds render error methods and rescue from statments for invalid/not found records

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,16 @@
 class ArtistsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_response
+
+
+
+    def render_invalid_response(invalid)
+        render json: { error: invalid.record.errors.messages}, status: :unprocessable_entity
+    end
+
+    def render_not_found_response
+        render json: { error: "Artist not found" }, status: :not_found
+      end
+end
+
 end
