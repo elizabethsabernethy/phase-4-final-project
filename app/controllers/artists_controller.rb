ed77d8d83925f93730dd06964a09f062cacfe48f
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-25 12:16:47 -0400

    adds strong params and built out index, show, and create methods

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,7 +3,26 @@ class ArtistsController < ApplicationController
     rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_response
 
 
-  
+    def index
+        artists = Artist.all.order(name: :asc)
+        render json: artists, include: :paintings
+    end
+
+    def show
+        artist = Artist.find(params[:id])
+        render json: artist, include: :paintings
+    end
+
+    def create
+        artist = Artist.create!(artist_params)
+        render json: artist, include: :paintings, status: :created
+    end
+
+    private
+
+    def artist_params
+        params.permit(:name, :username, :password_digest)
+    end
 
     def render_invalid_response(invalid)
         render json: { error: invalid.record.errors.messages}, status: :unprocessable_entity
