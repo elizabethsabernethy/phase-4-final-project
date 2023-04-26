f5bbd77fc1507047217ccc19234f0b5e672db7ae
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-26 01:52:51 -0400

    fixes multiple rendering of artist name using global varibale

--- client/src/ArtistPaintingCollection.js
+++ client/src/ArtistPaintingCollection.js
@@ -10,23 +10,27 @@ function ArtistPaintingCollection(){
         .then((paintings) => setPaintings(paintings))
     },[])
 
+    const artistName = paintings.map((painting)=>{
+        return painting.artist.name
+    })
+
     return(
         <div>
+            <div id="artist-name-container">
+                <h1>Paintings by {artistName[0]}</h1>
+            </div>
+            <div id="artist-paintings-container">
             {paintings.map((painting)=>{
-                return <div>
-                <div id="artist-name-container">
-                    <h1>Paintings by {painting.artist.name}</h1>
-                </div>
-                <div id="artist-paintings-container">
-                    <div className="painting" key={painting.id}>
+                return <div key={painting.id}>
+                    <div className="painting">
                         <img className="painting-img" src={painting.img_url} alt={painting.title} width="350px"></img>
                         <h3 className="painting-title">{painting.title}</h3>
                         <p className="painting-description">{painting.description}</p>
                         <h5>Displayed at {painting.museum.name}</h5>
                     </div>
                 </div>
-                </div>
             })}
+            </div>
         </div>
     )
 }
