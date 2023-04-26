109f8d8570ff8b1711011d6718c6bc5cad79d302
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-26 03:23:05 -0400

    adjusts show and create methods to include auhtorizations

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,19 +2,26 @@ class ArtistsController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
     rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_response
 
+    before_action :authorize, only: [:show]   
+
     def index
         artists = Artist.all.order(name: :asc)
         render json: artists, include: [:paintings, :museums]
     end
 
     def show
-        artist = Artist.find(params[:id])
+        artist = Artist.find_by(id: session[:user_id])
         render json: artist, include: [:paintings, :museums]
     end
 
     def create
-        artist = Artist.create!(artist_params)
+        user = Artist.create(artist_params)
+        if user.valid?
+            session[:user_id] = user.id
         render json: artist, include: [:paintings, :museums], status: :created
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
     end
 
     def update
@@ -32,7 +39,7 @@ class ArtistsController < ApplicationController
     private
 
     def artist_params
-        params.permit(:name, :username, :password_digest)
+        params.permit(:name, :username, :password, :password_confirmation)
     end
 
     def render_invalid_response(invalid)
@@ -41,5 +48,9 @@ class ArtistsController < ApplicationController
 
     def render_not_found_response
         render json: { error: "Artist not found" }, status: :not_found
-      end
+    end
+
+    def authorize
+        return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+    end
 end
