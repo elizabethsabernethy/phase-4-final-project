2a9b4b6341b93c3ae657b18062b3f4ff2734e5f8
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-26 00:51:47 -0400

    adds paintings.length to view paintings button

--- client/src/Museum.js
+++ client/src/Museum.js
@@ -12,7 +12,7 @@ function click(){
             <h3 className="museum-name">{museum.name}</h3>
             <h4 className="museum-location">{museum.location}</h4>
             <img className="museum-img" src={museum.img_url} alt={museum.name} width="350px"></img>
-            <button className="view-paintings" onClick={click}>View Paintings</button>
+            <button className="view-paintings" onClick={click}>View ({museum.paintings.length}) Paintings</button>
         </div>       
     )
 }
