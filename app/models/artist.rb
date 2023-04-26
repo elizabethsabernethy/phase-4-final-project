cdffbad9be2cab51cc76b4e124fe6d9aad4922ae
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-26 03:24:39 -0400

    moves session controller to controller folder, and adds has-secure-password to artist model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,5 @@
 class Artist < ApplicationRecord
+    has_secure_password
     has_many :paintings, dependent: :destroy
     has_many :museums, through: :paintings 
     validates :username, presence: true, uniqueness: true
