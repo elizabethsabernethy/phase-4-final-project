4c5bd2cc4f9aa14c2650b89f7833ade573342d04
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-26 01:07:41 -0400

    adds id to error message and styling in css

--- client/src/PageNotFound.js
+++ client/src/PageNotFound.js
@@ -2,7 +2,7 @@ function PageNotFound(){
 
     return(
         <div id="page-not-found-container">
-            <h1>We're sorry, the page you're looking for doesn't exist!</h1>
+            <h1 id='error-message'>We're sorry, the page you're looking for doesn't exist!</h1>
         </div>
     )
 }
