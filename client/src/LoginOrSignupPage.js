060411f18fcef74bb0a9cf3939baa8e21a9478f9
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-26 04:24:25 -0400

    adds function with setlogin ternary

--- client/src/LoginOrSignupPage.js
+++ client/src/LoginOrSignupPage.js
@@ -1,9 +1,35 @@
-function LoginOrSignupPage(){
-    return(
-        <div>
+import LoginForm from "./LoginForm";
+import SignupForm from "./SignupForm";
 
-        </div>
-    )
+function LoginOrSignupPage(){
+
+ const [showLogin, setShowLogin] = useState(true);
+
+  return (
+      <div>
+      {showLogin ? (
+        <>
+          <LoginForm onLogin={onLogin} />
+          <p>
+            Don't have an account? &nbsp;
+            <button onClick={() => setShowLogin(false)}>
+              Sign Up
+            </button>
+          </p>
+        </>
+      ) : (
+        <>
+          <SignupForm onLogin={onLogin} />
+          <p>
+            Already have an account? &nbsp;
+            <button onClick={() => setShowLogin(true)}>
+              Log In
+            </button>
+          </p>
+        </>
+      )}
+      </div>
+  );
 }
 
 export default LoginOrSignupPage;
