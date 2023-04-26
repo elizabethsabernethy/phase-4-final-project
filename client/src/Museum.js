19c39701ef166b5167149a820c16eb93431ce855
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-26 02:07:17 -0400

    adds ternary to prevent user from clicking view paintings button if museum has no paintings

--- client/src/Museum.js
+++ client/src/Museum.js
@@ -12,7 +12,8 @@ function click(){
             <h3 className="museum-name">{museum.name}</h3>
             <h4 className="museum-location">{museum.location}</h4>
             <img className="museum-img" src={museum.img_url} alt={museum.name} width="350px"></img>
-            <button className="view-paintings" onClick={click}>View ({museum.paintings.length}) Paintings</button>
+            {museum.paintings.length > 0? <button className="view-paintings" onClick={click}>View ({museum.paintings.length}) Paintings</button> : <p>Empty Collection</p>}
+            
         </div>       
     )
 }
