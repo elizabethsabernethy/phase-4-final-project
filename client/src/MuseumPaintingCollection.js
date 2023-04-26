055fcb17fc100b56963a9457b4c30887ce0fe5f2
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-26 01:42:27 -0400

    adjusts divs with id and classname to better style

--- client/src/MuseumPaintingCollection.js
+++ client/src/MuseumPaintingCollection.js
@@ -11,17 +11,19 @@ function MuseumPaintingCollection(){
     },[])
 
     return(
-        <div id="museum-paintings-container">
+        <div>
             {paintings.map((painting)=>{
                 return <div>
                 <div id="museum-name-container">
-                    <h1>The paintings at {painting.museum.name}</h1>
+                    <h1>{painting.museum.name} Collection</h1>
                 </div>
-                <div className="painting" key={painting.id}>
-                    <img className="painting-img" src={painting.img_url} alt={painting.title} width="350px"></img>
-                    <h3 className="painting-title">{painting.title}</h3>
-                    <h4>{painting.artist.name}</h4>
-                    <p className="painting-description">{painting.description}</p>
+                <div id="museum-paintings-container">
+                    <div className="painting" key={painting.id}>
+                        <img className="painting-img" src={painting.img_url} alt={painting.title} width="350px"></img>
+                        <h3 className="painting-title">{painting.title}</h3>
+                        <h4>{painting.artist.name}</h4>
+                        <p className="painting-description">{painting.description}</p>
+                    </div>
                 </div>
                 </div>
             })}
