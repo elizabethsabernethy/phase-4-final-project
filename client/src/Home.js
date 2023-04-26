d7c0f799ed004b5da87e4893281e756a208a0a88
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-26 02:59:05 -0400

    adds img and changes text and styling of homepage

--- client/src/Home.js
+++ client/src/Home.js
@@ -4,9 +4,14 @@ function Home(){
     return(
         <div id="home-container">
             <div className="name-container">
-                <h1>The Art of Collection</h1>
+                <h1>Off the Arts</h1>
             </div>
-            <p>Welcome to the Art of Collection, where you can view some of the worlds most famous paintings, find out which museum they belong to, view the incredible artists who created these works, and even submit your own works!</p>
+            <p>Welcome to the Off the Arts, where the art here is off the charts! Check out our featured museums, and artists if you want a more specific collection of paintings. Otherwise, head on over our feature paintings and marvel at their beauty. If you are an artist and would like to share your work, please free to join the community and add your own peices to our ever growing collections.</p>
+            <div id="home-img-container">
+                <img src="https://www.vangoghgallery.com/img/starry_night_full.jpg" alt="Vincent Van Gogh's Starry Night" width="600px" height="600px"></img>
+                <p>Starry Night by Vincent Van Gogh</p>
+            </div>
+            
         </div>
     )
 }
