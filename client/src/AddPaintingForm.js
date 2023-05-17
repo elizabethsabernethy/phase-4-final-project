e97747df4aadab415f281382305ba8732f610af7
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-05-17 14:59:56 -0400

    adds form for adding paitning through user

---
+++ client/src/AddPaintingForm.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom"
+
+function AddPaintingForm({user}){
+
+    const navigate = useNavigate()
+
+    const [title, setTitle] = useState("");
+    const [imgUrl, setImgUrl] = useState("");
+    const [description, setDescription] = useState("");
+    const [museumValue, setMuseumValue] = useState("");
+  
+    return(
+        <div>
+            <h1 className="name-container">Add Painting</h1>
+            <form>
+            <label htmlFor="title">Title</label>
+                <input
+                    type="text"
+                    id="title"
+                    autoComplete="off"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label htmlFor="imgUrl">Image URL</label>
+                <input
+                    type="text"
+                    id="imgUrl"
+                    autoComplete="off"
+                    value={imgUrl}
+                    onChange={(e) => setImgUrl(e.target.value)}
+                />
+                <label htmlFor="description">Painting description</label>
+                <input
+                    type="text"
+                    id="description"
+                    autoComplete="off"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                />
+                <label htmlFor="museum">Museum</label>
+                <select id="museum" name="museum" onChange={(e) => setMuseumValue(e.target.value)}>
+                    {user.museums.map((museum)=>{
+                        <option value={museumValue}>{museum.name}</option>
+                    })}
+                </select>
+            </form>
+        </div>
+    
+    )
+}
+
+export default AddPaintingForm;
