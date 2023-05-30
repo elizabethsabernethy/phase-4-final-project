d82e1c0db16ef3be3feea70120053117bec9f479
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-05-30 10:46:32 -0400

    adjusts users paintings display

--- client/src/UserPaintingCollection.js
+++ client/src/UserPaintingCollection.js
@@ -1,24 +1,15 @@
-import { useState, useEffect } from "react";
-import User from "./User";
+import { useEffect, useState } from "react";
 
 function UserPaintingCollection({user}){
-    const[paintings, setPaintings]= useState([])
+    const [img, setImg] = useState("")
+
+    const paintings = user.id? user.paintings: [];
 
     useEffect(()=>{
-        fetch(`http://localhost:3000/profile/${user.id}/paintings`)
-    .then((resp)=>{
-      if(resp.ok){
-        return resp.json()
-    .then((paintings)=>{
-      return setPaintings(paintings)})
-      }
-      setPaintings([])
-      })
+        paintings.map((painting)=>{
+            return setImg(painting.img_url)
+        })
     },[])
-
-    {console.log(paintings)}
-
-    //find work around for user.paintings, etc when user={}
 
     return(
         <div>
@@ -30,10 +21,9 @@ function UserPaintingCollection({user}){
                 return <div key={painting.id}>
                     {paintings.length < 1 ? "Please add paintings" :
                     <div className="painting">
-                        <img className="painting-img" src={painting.img_url} alt={painting.title} width="350px"></img>
+                        <img className="painting-img" src={img} onError = {() => setImg("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Image_not_available.png/640px-Image_not_available.png")} alt={painting.title} width="350px"></img>
                         <h3 className="painting-title">{painting.title}</h3>
                         <p className="painting-description">{painting.description}</p>
-                        <h5>Displayed at {painting.museum.name}</h5>
                     </div>
                     }
                 </div>
