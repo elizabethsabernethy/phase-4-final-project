ee7219802cd1fcf869d4de931e8be66393962571
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-06-13 18:21:04 -0400

    updates user collection to include delete and edit buttons, and changes button styling

--- client/src/UserPaintingCollection.js
+++ client/src/UserPaintingCollection.js
@@ -4,6 +4,14 @@ function UserPaintingCollection({user, newPainting}){
 
     const paintings = user.id? user.paintings : []
 
+    function editPainting(){
+        console.log("edit")
+    }
+
+    function deletePainting(){
+        console.log("delete")
+    }
+
     return(
         <div>
             <div className="name-container">
@@ -13,7 +21,11 @@ function UserPaintingCollection({user, newPainting}){
             {paintings.map((painting)=>{
                 return <div key={painting.id}>
                     {paintings.length < 1 ? "Please add paintings" :
+                    <div>
                     <Painting painting={painting}></Painting>
+                    <button onClick={editPainting} className="painting-button">Edit Painting</button>
+                    <button onClick={deletePainting} className="painting-button">Delete Painting</button>
+                    </div>
                     }
                 </div>
             })}
