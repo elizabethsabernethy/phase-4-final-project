dafada005885532d02cd5fa6e3c050971f0c3f5f
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-06-15 11:08:57 -0400

    adds a view artists button for each museum, and updates notes to reflect improvements and new issues

--- client/src/Museum.js
+++ client/src/Museum.js
@@ -1,19 +1,37 @@
+import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
 function Museum({museum}){
+const[show, setShow] = useState(false)
+
 const navigate = useNavigate()
 
 function click(){
     navigate(`${museum.id}/paintings`)
 }
 
+function showArtists(){
+    setShow((show)=> !show)
+}
+
+console.log(museum.artists)
+
     return(
         <div className="museum">
             <h3 className="museum-name">{museum.name}</h3>
             <h4 className="museum-location">{museum.location}</h4>
             <img className="museum-img" src={museum.img_url} alt={museum.name} width="350px"></img>
-            {museum.paintings.length > 0? <button className="view-paintings" onClick={click}>View ({museum.paintings.length}) Paintings</button> : <p>Empty Collection</p>}
-            
+            {museum.paintings.length > 0? 
+            <div>
+                <button className="view-paintings" onClick={click}>View ({museum.paintings.length}) Paintings</button>
+                <button onClick={showArtists} className="view-paintings">{!show ? 'View Featured Artists' : 'Hide Artists'}</button>
+            <div hidden={!show}>
+                {museum.artists.map((artist)=>{
+                    return <h4>{artist.name}</h4>
+                })}
+            </div>
+            </div>: 
+            <p>Empty Collection</p>}
         </div>       
     )
 }
